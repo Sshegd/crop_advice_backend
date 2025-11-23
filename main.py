a1a392ea3b3a821cd8cedaac24c2d6ae4d9c7aa1
Sshegd <153484187+Sshegd@users.noreply.github.com>
2025-11-23 11:11:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -128,9 +128,13 @@ async def advice_new(payload: dict):
 
 # --------------- PORT FOR RAILWAY LOCAL TESTING --------- #
 # Only needed if you run `python main.py` locally
+import uvicorn
+import os
+
 if __name__ == "__main__":
-    import uvicorn
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
+
+
 
 
