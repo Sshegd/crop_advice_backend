96526a2c7693f2e9dfde7a2b4a952ed9b72c3755
Sshegd <153484187+Sshegd@users.noreply.github.com>
2025-11-23 11:29:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -130,8 +130,9 @@ async def advice_new(payload: dict):
 # Only needed if you run `python main.py` locally
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 8080))
     uvicorn.run("main:app", host="0.0.0.0", port=port)
+
 
 
 
